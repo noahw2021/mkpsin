1d64ecdffa6f530ac98788a636d9e31f2172e490
Noah Wooten <noah.wooten@icloud.com>
2022-02-14 13:46:54 -0500

    finish markdown engine

--- mkpsin/main.c
+++ mkpsin/main.c
@@ -16,36 +16,13 @@
 #define ArgumentBody(Source, Name) if (!strcmp(Source, Name))
 
 int main(int argc, char** argv) {
-	mk_init(MKI_LINE_LF);
-	mkdoc_t* Document = mk_newdoc();
-	mktable_t* Table = mk_newtable();
-	
-	mkdt_addheader(Table, "Header A", 0);
-	mkdt_addheader(Table, "Header B", 1);
-	mkdt_addheader(Table, "Header C", 2);
-	mkdt_addfield(Table, "Test #00", 0, 0);
-	mkdt_addfield(Table, "Test #01", 0, 1);
-	mkdt_addfield(Table, "Test #02", 0, 2);
-	mkdt_addfield(Table, "Test #03", 0, 3);
-	mkdt_addfield(Table, "Test #10", 1, 0);
-	mkdt_addfield(Table, "Test #11", 1, 1);
-	mkdt_addfield(Table, "Test #12", 1, 2);
-	mkdt_addfield(Table, "Test #13", 1, 3);
-	mkd_addtable(Document, Table);
-	
-	char* CompiledDocument = mk_compile(Document);
-	printf("%s", CompiledDocument);
-	free(CompiledDocument);
-	mk_shutdown();
-	
 	psin_init();
 	main_loadinst();
 	
 	char* SourceArgument = argv[1];
 	if (argc < 2){
 		ui_help(argc, argv);
-		psin_shutdown();
-		return 0;
+		goto GlobalReturn;
 	}
 	ArgumentBody(SourceArgument, "-g") {
 		ui_generate(argc, argv);
@@ -56,6 +33,7 @@ int main(int argc, char** argv) {
 	ArgumentBody(SourceArgument, "-h") {
 		ui_help(argc, argv);
 	}
+GlobalReturn:
 	psin_shutdown();
 	return 0;
 }
